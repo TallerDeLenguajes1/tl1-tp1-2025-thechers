233b9756ad9b93dc3492a6b3174a3f42f6661d42
Matias <matiass.fm13@gmail.com>
2025-03-31 16:10:00 -0300

    Mostrando info de punteros

--- tp1_1.c
+++ tp1_1.c
@@ -2,7 +2,17 @@
 
 int main (){
     
-    printf("Hola Mundo");
+    printf("Hola Mundo\n");
+
+    int num1 = 1, *p_num1;
+
+    p_num1 = &num1;
+
+    printf("Contenido del puntero: %d\n", *p_num1);
+    printf("Direccion de memoria almacenada por el puntero: %p\n", p_num1);
+    printf("Direccion de memoria de la variable: %p\n", &num1);
+    printf("Direccion de memoria de puntero: %p\n", &p_num1);
+    printf("Tamaño de memoria utilizado por la variable: %d\n", sizeof(num1));
 
     return 0;
 }
